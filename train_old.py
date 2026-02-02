da7c16c5316ee2337b8cf52118e671a80a454260
Stefan Moser <e12025955@student.tuwien.ac.at>
2026-02-02 13:57:11 +0100

    Add files via upload

---
+++ train_old.py
@@ -0,0 +1,130 @@
+import argparse
+import os
+import copy
+
+import torch
+from torch import nn
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+from torch.utils.data.dataloader import DataLoader
+from tqdm import tqdm
+
+from models import SRCNN
+from datasets import TrainDataset, EvalDataset
+from utils import AverageMeter, calc_psnr
+
+
+if __name__ == '__main__':
+    # Parse input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train-file', type=str, required=True)
+    parser.add_argument('--eval-file', type=str, required=True)
+    parser.add_argument('--outputs-dir', type=str, required=True)
+    parser.add_argument('--scale', type=int, default=3)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--batch-size', type=int, default=16)
+    parser.add_argument('--num-epochs', type=int, default=400)
+    parser.add_argument('--num-workers', type=int, default=8)
+    parser.add_argument('--seed', type=int, default=123)
+    args = parser.parse_args()
+
+    # Create output directory
+    args.outputs_dir = os.path.join(args.outputs_dir, 'x{}'.format(args.scale))
+
+    if not os.path.exists(args.outputs_dir):
+        os.makedirs(args.outputs_dir)
+
+    # Set device
+    cudnn.benchmark = True
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    torch.manual_seed(args.seed)
+
+    # Initialize model, loss function, and optimizer
+    model = SRCNN().to(device)  # Define the model
+    criterion = nn.MSELoss()    # Define the loss function => Mean Squared Error
+
+    optimizer = optim.Adam([
+        {'params': model.conv1.parameters()},
+        {'params': model.conv2.parameters()},
+        {'params': model.conv3.parameters(), 'lr': args.lr * 0.1}
+    ], lr=args.lr)
+
+
+    # Load train dataset
+    train_dataset = TrainDataset(args.train_file)
+    train_dataloader = DataLoader(dataset=train_dataset,
+                                  batch_size=args.batch_size,
+                                  shuffle=True,
+                                  num_workers=args.num_workers,
+                                  pin_memory=True,
+                                  drop_last=True)
+    
+    # Load eval dataset
+    eval_dataset = EvalDataset(args.eval_file)
+    eval_dataloader = DataLoader(dataset=eval_dataset, batch_size=1)
+
+
+    best_weights = copy.deepcopy(model.state_dict())
+    best_epoch = 0
+    best_psnr = 0.0
+
+    # Train the model
+    for epoch in range(args.num_epochs):
+        model.train()
+        epoch_losses = AverageMeter()
+
+        with tqdm(total=(len(train_dataset) - len(train_dataset) % args.batch_size)) as t:
+            t.set_description('epoch: {}/{}'.format(epoch, args.num_epochs - 1))
+
+            for data in train_dataloader:
+                # Load training input and target images
+                inputs, labels = data
+
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                
+                # make predictions
+                preds = model(inputs)
+
+                # Compute loss
+                loss = criterion(preds, labels)
+
+                epoch_losses.update(loss.item(), len(inputs))
+
+                # Backpropagation and optimization
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                t.set_postfix(loss='{:.6f}'.format(epoch_losses.avg))
+                t.update(len(inputs))
+
+        torch.save(model.state_dict(), os.path.join(args.outputs_dir, 'epoch_{}.pth'.format(epoch)))
+
+        model.eval()
+        epoch_psnr = AverageMeter()
+
+
+        for data in eval_dataloader:
+            # load eval input and target images
+            inputs, labels = data
+
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            with torch.no_grad():
+                preds = model(inputs).clamp(0.0, 1.0)
+
+            #evaluate performance via psnr
+            epoch_psnr.update(calc_psnr(preds, labels), len(inputs))
+
+        print('eval psnr: {:.2f}'.format(epoch_psnr.avg))
+
+        if epoch_psnr.avg > best_psnr:
+            best_epoch = epoch
+            best_psnr = epoch_psnr.avg
+            best_weights = copy.deepcopy(model.state_dict())
+
+    print('best epoch: {}, psnr: {:.2f}'.format(best_epoch, best_psnr))
+    torch.save(best_weights, os.path.join(args.outputs_dir, 'best.pth'))
